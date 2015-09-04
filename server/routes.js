30fa9b9c53289473333d7038ad0cb79e8d3c54b5
excalliburbd <excalliburbd@gmail.com>
2015-09-04 16:43:06 +0600

    Simple contact us for with RESTful interaction

--- server/routes.js
+++ server/routes.js
@@ -10,6 +10,7 @@ var path = require('path');
 module.exports = function(app) {
 
   // Insert routes below
+  app.use('/api/inbox', require('./api/inbox'));
   app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
 
